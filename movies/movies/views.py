6731e7c6596c35f88b572540f88ea6f3b59bdf77
mooc-31 <mooc-31@mooc31-desktop.(none)>
2014-02-17 17:21:05 +0530

    new project

--- movies/movies/views.py
+++ movies/movies/views.py
@@ -6,13 +6,16 @@ from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
 def load(request, addr):
+    if not addr:
+        return render(request, 'blank.html')
     title_list = addr.strip('/').split("/")
+    
     with open('json2.json', 'r') as json_file:
         node = json.load(json_file)
         
         # taking base url as "Movies"
         if title_list[-1] == 'Movies':
-            return render(request, 'parent.html', {'node': node, 'addr': addr})
+            return render(request, 'parent.html', {'node': node})
         for child in node['movies']:
             if child['title'] == title_list[-1]:
                 node = child
