c2f7a38ec5d08e4acb881640e9a55af9e778f0ce
codemania113 <paridhi2011@gmail.com>
2014-02-18 19:05:12 +0530

    after review 1

--- movies/movies/views.py
+++ movies/movies/views.py
@@ -12,15 +12,16 @@ def load(request, addr):
     
     with open('json2.json', 'r') as json_file:
         node = json.load(json_file)
-        
+        check = 0
         # taking base url as "Movies"
         if title_list[-1] == 'Movies':
             return render(request, 'parent.html', {'node': node})
+
         for child in node['movies']:
             if child['title'] == title_list[-1]:
                 node = child
-                break
-        if node['title'] != title_list[-1]:
-            return HttpResponseNotFound('<h1>Page Not Found</h1>')
+                check = 1
+                return render(request, 'child.html', {'node': node})
         
-        return render(request, 'child.html', {'node': node})
+        if not check:
+            return HttpResponseNotFound('<h1>Page Not Found</h1>')
